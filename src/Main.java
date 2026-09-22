a0ccdce50cafe31da2c63b14d98ba57e5becdf08
minhtrannguyendevs <minhtrannguyen.devs@gmail.com>
2026-09-22 11:10:30 +0700

    +1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 import javax.swing.SwingUtilities;
 import views.LoginFrame;
-
-/**
- * Điểm khởi chạy duy nhất của chương trình.
- * Mở màn hình đăng nhập, đăng nhập thành công thì LoginFrame tự mở HomeFrame.
- */
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
